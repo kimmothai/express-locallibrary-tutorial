aec5249c595811e07f8b3c245614990758dbfdcc
kimmothai <kqmthai@gmail.com>
2021-07-19 00:19:41 +0300

    Removed logger

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
 app.use(compression())
-app.use(logger('dev')) // TODO do something about this
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
